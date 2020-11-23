df753889db795e09f2711481196ae2f32c60b3ad
AbCreativeAmid <hussainiabdullah786@gmail.com>
2020-11-23 09:09:47 +0430

    Code Enhanced After Evaluation

--- groups_insert.py
+++ groups_insert.py
@@ -5,7 +5,7 @@ def db_connect():
         connection = None
 
         try:
-            connection = psycopg2.connect(user="",password="",host="",port="",database="")
+            connection = psycopg2.connect(user="ubuntu  ",password="12345",host="127.0.0.1",port="5432",database="script")
             
         except (Exception, psycopg2.Error) as error :
             print ("Error while connecting to PostgreSQL", error)
@@ -15,26 +15,27 @@ def db_connect():
 def insert_groups():
     """ read the groups from excel and distinct them and insert them to the database
         """
-        os.chdir(os.path.dirname(__file__))
-        work_book = xlrd.open_workbook("items.xlsx")
-        sheet = work_book.sheet_by_name("List of Items")
-        groups = []
+    os.chdir(os.path.dirname(__file__))
+    work_book = xlrd.open_workbook("items.xlsx")
+    sheet = work_book.sheet_by_name("List of Items")
+    groups = []
 
-        for i in range(sheet.nrows):
-            row = sheet.row(i)
-            item = ()
-            if row[3].value != '':
-                item = (str(row[3].value),)
-                if item not in groups:
-                    groups.append(item)
-        connection = db_connect()
-        if connection:
-            cursor       = connection.cursor()
-            for g in groups:
-                insert_query = """INSERT INTO product_category(name) VALUES(%s)"""
-                cursor.execute(insert_query,g)
-            connection.commit()
-            cursor.close()
-            connection.close()
-            print(" Groups Successfully inserted")
+    for i in range(sheet.nrows):
+        row = sheet.row(i)
+        item = ()
+        if row[3].value != '':
+            item = (str(row[3].value),)
+            if item not in groups:
+                groups.append(item)
+    connection = db_connect()
+    if connection:
+        cursor       = connection.cursor()
+        for g in groups:
+            insert_query = """INSERT INTO product_category(name) VALUES(%s)"""
+            cursor.execute(insert_query,g)
+        connection.commit()
+        cursor.close()
+        connection.close()
+        print(" Groups Successfully inserted")
+        
 insert_groups()
--- script.py
+++ script.py
@@ -15,7 +15,7 @@ class Write_products():
         connection = None
 
         try:
-            connection = psycopg2.connect(user="",password="",host="",port="",database="")
+            connection = psycopg2.connect(user="ubuntu",password="12345",host="127.0.0.1",port="5432",database="script")
             
         except (Exception, psycopg2.Error) as error :
             print ("Error while connecting to PostgreSQL", error)
@@ -34,29 +34,20 @@ class Write_products():
             work_book = xlrd.open_workbook(os.path.dirname(__file__)+"/items.xlsx")
             fileExist = True
         except:
-            print("file not found! \n please put the script file in excel file directory")
+            print("done")
 
         if fileExist:
             sheet = work_book.sheet_by_name("List of Items")
             groups = self.get_groups()
-            connection = self.db_connect()
             imgpath = os.path.dirname(__file__)+"/images/"
-            if groups and connection:
-                cursor = connection.cursor()
-                for i in range(1,sheet.nrows):
+            if groups:
+                for i in range(1,500):
                     row = sheet.row(i)
                     item = {}
-                    name            = "No Name" if row[1].value == '' else str(row[1].value)
-                    part_no         = "No Part No" if  row[1].value == '' else str(row[2].value)
-                    category_id     = groups["All"] if row[3].value =='' else groups[row[3].value]
-                    default_code    =  "No default Code" if  row[1].value == '' else str(row[0].value)
-                    record          = (name,category_id,part_no,default_code)
-                
-                    query = """INSERT INTO product_template(name,categ_id,part_no,default_code,type,uom_id,uom_po_id,tracking,active)
-                                Values(%s,%s,%s,%s,'consu',1,1,'none',TRUE) returning id"""
-                    cursor.execute(query,record)
-                    id = cursor.fetchone()[0]
-                    item["id"] = id
+                    item["name"]            = "No Name" if row[1].value == '' else str(row[1].value)
+                    item["part_no"]         = "No Part No" if  row[1].value == '' else str(row[2].value)
+                    item["categ_id"]        = groups["All"] if row[3].value =='' else groups[row[3].value]
+                    item["default_code"]    = "No default Code" if  row[1].value == '' else str(row[0].value)
                     item["image_1920"] = ""
                     if row[4].value != '' and row[4].value != 'N/A':
                         if os.path.isfile(imgpath+row[4].value):
@@ -64,33 +55,36 @@ class Write_products():
                             item["image_1920"] = base64.b64encode(f.read()).decode('utf-8')
                             
                     itemlist.append(item)
-
-                connection.commit()  
-                cursor.close()
-                connection.close()
         return itemlist    
         
 
     def connect_odoo(self):
         """ connect to odoo through xml rpc and update the images of products """
-        url =""
-        db = ""
-        username = ""
-        password = ""
+        url ="http://127.0.0.1:8000"
+        db = "script"
+        username = "admin"
+        password = "admin"
         common = None
         try:
             common   = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
-        except:
-            print("not connected")
+        except Exception as error:
+            print(error)
+            
         
         if common:
             uid      = common.authenticate(db,username,password,{})
             models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
             items = self.read_excel()
-            if len(items)>1:
-                for item in items:
-                    models.execute_kw(db,uid,password,"product.template","write",[[item["id"]],{"image_1920":item["image_1920"]}])
+            iterates = len(items)//25
 
+            for i in range(iterates+1):
+                st = i*25
+                end = st+25
+                if i == iterates:
+                    insert_items = items[st:]
+                else:
+                    insert_items = items[st:end]
+                models.execute_kw(db,uid,password,"product.template","create",[insert_items])
     
     def get_groups(self):
         """ read the categories or groups from the database
